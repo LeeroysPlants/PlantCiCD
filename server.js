16fb44cdafbdb18f8d46393bf1794e73cd73f0c7
Noah Wenig <nwenig@trinity.edu>
2025-03-18 10:07:16 -0500

    port will hopefully work on azure or local development

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const port = process.env.PORT; 
+const port = process.env.PORT || 3000; 
 
 app.use(cookieParser());
 app.use(express.static('public'));
