52f0b3be0704a927ac8ab15dac76284a511661f4
Noah Wenig <nwenig@trinity.edu>
2025-03-20 15:21:42 -0500

    Sql code now runs
    didn't like types not being declared in the auto increment key
    insert was too long for varchar 10 data type

--- database.sql
+++ database.sql
@@ -1,25 +1,24 @@
-create table device (
-    device_id primary key auto increment,
-    type_ varchar(10) not null,
-    name_ varchar(10) not null,
-    location_ varchar(10)
+CREATE TABLE device (
+    device_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    type_ VARCHAR(10) NOT NULL,
+    name_ VARCHAR(10) NOT NULL,
+    location_ VARCHAR(10)
 );
 
-
-create table Sensor (
-    sensor_id primary key auto increment,
-    name varchar(10) not null,
-    desc_ varchar(40),
-    device_id not null,
-    foreign key (device_id) references device(device_id)
+CREATE TABLE sensor (
+    sensor_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(10) NOT NULL,
+    description VARCHAR(40),
+    device_id BIGINT NOT NULL,
+    FOREIGN KEY (device_id) REFERENCES device(device_id)
 );
 
-create table Measurement(
-    meas_id primary key auto increment,
-    measurement float not null,
-    time_ time not null,
-    sensor_id bigint not null,
-    foreign key (sensor_id) references sensor(sensor_id)
+CREATE TABLE measurement (
+    meas_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    measurement FLOAT NOT NULL,
+    time_ DATETIME NOT NULL,
+    sensor_id BIGINT NOT NULL,
+    FOREIGN KEY (sensor_id) REFERENCES sensor(sensor_id)
 );
 
-insert into device(type_, name_) values("Raspberry Pi 5", "Pi");
+INSERT INTO device (type_, name_) VALUES ("devPi", "Pi");
