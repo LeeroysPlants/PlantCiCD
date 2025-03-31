45130c7a5201309c7899366e4ecd12ec761f65a1
Noah Wenig <nwenig@trinity.edu>
2025-03-31 13:05:54 -0500

    added more logs in mysql connection, removed env db variables

--- db.js
+++ db.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql2/promise')
 require('dotenv').config();
-
+try{
+	console.log("host: " + process.env.DATABASE_HOST); 
+	console.log("user: " + process.env.DATABASE_USER); 
 const connection = mysql.createConnection({
 	host : process.env.DATABASE_HOST,
 	user : process.env.DATABASE_USER,
 	password : process.env.DATABASE_PASSWORD,
 	database : process.env.DATABASE_NAME
-});
-
-// connection.connect((err => {
-//     if(err) throw err;
-//     console.log(`MySQL Connected`);
-// }));
-
-exports.db_connection = connection
+        });
+		exports.db_connection = connection
+	}
+	catch(error){
+		console.error('MySQL Connection Error:', error.message);
+	}
